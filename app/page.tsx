6e61d1befb05e1b638b12e96c14cb40a99699974
Nam <namdeveloper.ca@gmail.com>
2023-11-21 14:59:15 +0700

    Style Profile Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ContactCard from '@/components/ContactCard'
 import Footer from '@/components/Footer'
+import MobileProfileCard from '@/components/MobileProfileCard'
 import ProfileCard from '@/components/ProfileCard'
 import ServiceCard from '@/components/ServiceCard'
 import TagList from '@/components/TagList'
@@ -8,12 +9,23 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='w-full max-w-[1120px] mx-auto my-0 px-4 py-0'>
-      <div className='grid grid-cols-12 gap-6 md:grid-cols-12 md:gap-4 lg:gap-6 py-8'>
-        <div className='hidden lg:block overflow-scroll sticky lg:col-span-2 top-[63px] scrollbar-none h-[calc(100vh-73px)]'>
+      <div className='grid-cols-12 grid gap-6 px-0 py-8 md:px-0 md:py-2'>
+        <div className='hidden md:block md:overflow-scroll md:sticky md:grid-col-span-2 md:top-16 md:scrollbar-hidden h-[calc(100vh-73px)'>
           <TagList />
         </div>
-        <div className='col-span-12 lg:col-span-7'>Profile</div>
-        <div className='hidden lg:block overflow-scroll sticky lg:col-span-3 top-[63px] scrollbar-none h-[calc(100vh-73px)]'>
+        <div className='col-span-12 lg:col-span-7'>
+          <MobileProfileCard />
+          {/* <SearchInput value={q} onChange={(e) => setQ(e.target.value)} /> */}
+          <div className='block lg:hidden'>
+            <TagList />
+          </div>
+          {/* <FeedHeader />
+          <PostList q={q} /> */}
+          <div className='pt-4 lg:hidden'>
+            <Footer />
+          </div>
+        </div>
+        <div className='scrollbar-hidden hidden lg:block overflow-scroll sticky lg:col-span-3 top-[63px] scrollbar-none h-[calc(100vh-73px)]'>
           <ProfileCard />
           <ServiceCard />
           <ContactCard />
---
+++ components/MobileProfileCard.tsx
@@ -0,0 +1,33 @@
+import { CONFIG } from '@/site.config'
+import Image from 'next/image'
+import React from 'react'
+
+type Props = {
+  className?: string
+}
+
+const MobileProfileCard: React.FC<Props> = () => {
+  return (
+    <div className='block lg:hidden'>
+      <div className='mb-3 p-1'>💻 Profile</div>
+      <div className='bg-[rgb(40,40,40)] mb-4 p-2 rounded-2xl'>
+        <div className='flex gap-2 items-center'>
+          <Image
+            src={CONFIG.profile.image}
+            width={90}
+            height={90}
+            className='relative'
+            alt='profile_image'
+          />
+          <div className='h-fit'>
+            <div className='text-xl leading-7 italic font-bold'>{CONFIG.profile.name}</div>
+            <div className='text-sm leading-5 mb-2'>{CONFIG.profile.role}</div>
+            <div className='text-sm leading-5'>{CONFIG.profile.bio}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MobileProfileCard
