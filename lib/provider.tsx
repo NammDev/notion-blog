d9fbb2dec0e033feef0cbd9c3a28b914b0f7a1a0
Nam <namdeveloper.ca@gmail.com>
2023-11-23 19:07:26 +0700

    Remove react query devtool

--- lib/provider.tsx
+++ lib/provider.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function Providers({ children }: React.PropsWithChildren) {
   const [client] = React.useState(new QueryClient())
@@ -10,7 +9,6 @@ function Providers({ children }: React.PropsWithChildren) {
   return (
     <QueryClientProvider client={client}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: 'morethan-log',
-    description: 'welcome to morethan-log!',
+    title: 'Notion Blog',
+    description: 'welcome to Notion blog!',
   },
 
   // CONFIG configration (required)
