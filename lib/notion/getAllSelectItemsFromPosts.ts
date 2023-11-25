b60cdfd0b2c6fd25fe431572b493b2030e8c5fe3
Nam <namdeveloper.ca@gmail.com>
2023-11-25 16:46:19 +0700

    fix bug

--- lib/notion/getAllSelectItemsFromPosts.ts
+++ lib/notion/getAllSelectItemsFromPosts.ts
@@ -1,9 +1,6 @@
-import { TPosts } from "src/types"
+import { TPosts } from '@/types'
 
-export function getAllSelectItemsFromPosts(
-  key: "tags" | "category",
-  posts: TPosts
-) {
+export function getAllSelectItemsFromPosts(key: 'tags' | 'category', posts: TPosts) {
   const selectedPosts = posts.filter((post) => post?.[key])
   const items = [...selectedPosts.map((p) => p[key]).flat()]
   const itemObj: { [itemName: string]: number } = {}
